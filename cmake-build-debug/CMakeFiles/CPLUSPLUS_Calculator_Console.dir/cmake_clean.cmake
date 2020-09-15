596b08ccc13484fd2b86d5cf2942456cf42d67f5
Parallels <samuelvillalvalijo@gmail.com>
2020-09-15 15:54:11 -0500

    first run of scratch.cpp

---
+++ cmake-build-debug/CMakeFiles/CPLUSPLUS_Calculator_Console.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPLUSPLUS_Calculator_Console.dir/scratch.cpp.o"
+  "CPLUSPLUS_Calculator_Console"
+  "CPLUSPLUS_Calculator_Console.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPLUSPLUS_Calculator_Console.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- scratch.cpp
+++ scratch.cpp
@@ -1,3 +1,18 @@
 //first calculator program, logs out output to the console.
 
-#include iostream
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int i= 5;
+    int j = 6;
+    int g;
+    g = i+ j;
+
+    cout << "I is equal to: "<< i << endl;
+    cout << "J is equal to: "<< j << endl;
+    cout << "G is equal to I + J: "<< g << endl;
+
+    return 0;
+}
